e67275242df0e2205894e37f8f07a38830215ff5
FabricioSpiritosi <fspiritosi@gmail.com>
2022-11-23 22:37:38 -0300

    agregando homeworks

--- 06-EstructuraDeDatos-III/homework/testingPersona.js
+++ 06-EstructuraDeDatos-III/homework/testingPersona.js
@@ -1,66 +1,54 @@
-"use strict";
-
-/*
- Implementar la clase BinarySearchTree, definiendo los siguientes métodos recursivos:
-  - size: retorna la cantidad total de nodos del árbol
-  - insert: agrega un nodo en el lugar correspondiente
-  - contains: retorna true o false luego de evaluar si cierto valor existe dentro del árbol
-  - depthFirstForEach: recorre el árbol siguiendo el orden depth first (DFS) en cualquiera de sus variantes, según se indique por parámetro ("post-order", "pre-order", o "in-order"). Nota: si no se provee ningún parámetro, hará el recorrido "in-order" por defecto.
-  - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
-
-  El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
-*/
 function BinarySearchTree(value) {
-  this.value = value;
-  this.left = null;
-  this.right = null;
+  this.data = value,
+  this.rigth = null,
+  this.left = null
 }
-
+// 
 BinarySearchTree.prototype.size = function(){
   //arbol es una hoja
   if(this.left === null && this.right === null){
     return 1;
   }
-
+// 
   //tiene un solo hijo a izq
   if(this.left !== null && this.right === null){
     return 1 + this.left.size();
   }
-
+// 
   //tiene un solo hijo a der
   if(this.left === null && this.right !== null){
     return 1 + this.right.size();
   }
-
+// 
   //tiene ambos hijos
   if(this.left !== null && this.right !== null){
     return 1 + this.left.size() + this.right.size();
   }
-};
-
+}
+// 
 BinarySearchTree.prototype.insert = function(valor){
-  //si es mayor --> der
-  if(valor >= this.value){
-    if(this.right !== null){
-      this.right.insert(valor);
-    } else {
-      this.right = new BinarySearchTree(valor);
+    //si es mayor --> der
+    if(valor >= this.value){
+      if(this.right !== null){
+        this.right.insert(valor);
+      } else {
+        this.right = new BinarySearchTree(valor);
+      }
     }
-  }
-  //si es menor --> izq
-  if(valor < this.value){
-    if(this.left !== null){
-      this.left.insert(valor);
-    } else {
-      this.left = new BinarySearchTree(valor);
+    //si es menor --> izq
+    if(valor < this.value){
+      if(this.left !== null){
+        this.left.insert(valor);
+      } else {
+        this.left = new BinarySearchTree(valor);
+      }
     }
-  }
 };
-
+// 
 BinarySearchTree.prototype.contains = function(valor){
   //si es el root
   if(valor === this.value) return true;
-
+// 
   //si es mayor que el nodo
   if(valor > this.value){
     if(this.right === null){
@@ -78,10 +66,10 @@ BinarySearchTree.prototype.contains = function(valor){
     }
   }
 };
-
+// 
 BinarySearchTree.prototype.depthFirstForEach = function(cb, order){
-  //inorder
-  if(order === 'in-order' || !order){   //izq - padre - der
+   //inorder
+   if(order === 'in-order' || !order){   //izq - padre - der
     if(this.left !== null){
       this.left.depthFirstForEach(cb, order);
     }
@@ -107,32 +95,22 @@ BinarySearchTree.prototype.depthFirstForEach = function(cb, order){
     cb(this.value);
   }
 }
-
+// 
 BinarySearchTree.prototype.breadthFirstForEach = function(cb, array){
   if(!array){
     var array = [];
   }
   cb(this.value);
-
+// 
   if(this.left !== null){
     array.push(this.left);
   }
-
+// 
   if(this.right !== null){
     array.push(this.right);
   }
-
+// 
   if(array.length > 0){
     array.shift().breadthFirstForEach(cb, array);
   }
-
 }
-
-
-
-// No modifiquen nada debajo de esta linea
-// --------------------------------
-
-module.exports = {
-  BinarySearchTree,
-};
--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -7,6 +7,21 @@ function factorear(num) {
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
 
+  let arr = [1];
+  let factor = 2;
+
+  while (num !== 1) {
+    if(num % factor === 0){
+      arr.push(factor);
+      num = num / factor;
+
+    }else{
+      factor++
+    }
+  }
+  
+  return arr
+
 }
 
 function bubbleSort(array) {
@@ -14,7 +29,21 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  let flag = true;
 
+  while(flag){
+    flag = false;
+
+    for(var i = 0; i < array.length -1; i++){
+      if(array[i] > array[i+1]){
+        let aux = array[i];
+        array[i] = array[i+1];
+        array[i+1] = aux;
+        flag = true
+      }
+    }
+  }
+  return array
 }
 
 
@@ -24,6 +53,25 @@ function insertionSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  /*Logica del Insertion Sort
+  
+  Compara el primer elemento con el segundo ---> si esta ordenado no hace nada, si no, los cambia.
+  Segundo, compara el segundo con el tercero, si el tercero es mas chico, lo cambia con el segundo, y vuelve a comparar el primero con el segundo
+
+
+  */
+
+  for (let i = 1; i < array.length; i++) {
+    let j = i-1;
+    var aux = array[i];
+    while (j >= 0 && array[j] > aux) {
+      array[j + 1] = array[j];
+      j--; 
+    }
+    array[j+1] = aux
+  }
+  return array
+
 }
 
 
@@ -32,7 +80,18 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(var i = 0; i < array.length - 1; i++){
+    let min = i;
+    for(var j = i; j < array.length; j++){
+      if(array[j] < array[min]){
+        min = j;
+      }
+    }
+    let aux = array[i];
+    array[i] = array[min];
+    array[min] = aux;
+  }
+  return array;
 }
 
 
